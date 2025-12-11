aa525eec41b11a39554376349c2e3c7670a66e52
ekc0106 <ekc0106@naver.com>
2025-12-12 03:28:32 +0900

    v=4로 수정

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2025-12-11T17:43:21.388Z
+// Build Time: 2025-12-11T18:02:48.599Z
 export const APP_VERSION = {
-  version: '1765475001388',
-  buildTime: '2025-12-11T17:43:21.388Z',
-  hash: 'k48svizseqs'
+  version: '1765476168599',
+  buildTime: '2025-12-11T18:02:48.599Z',
+  hash: '2n6x03lfyr7'
 };
 
 export default APP_VERSION;
