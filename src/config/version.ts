3d5ca1397fadf93dd204ad4c5be83d98da705230
ekc0106 <ekc0106@naver.com>
2025-12-11 23:45:07 +0900

    Feat: Vimeo 영상 전체화면 지원 개선
    - 카카오톡 인앱브라우저에서 전체화면 버튼이 보이지 않는 문제 해결
    - Vimeo iframe에 fullscreen 관련 파라미터 추가
    - 별도의 전체화면 버튼 구현 (Vimeo 원본 페이지로 이동)
    - 모바일 환경에서 더 나은 영상 시청 경험 제공

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2025-12-11T13:50:01.313Z
+// Build Time: 2025-12-11T14:44:18.559Z
 export const APP_VERSION = {
-  version: '1765461001313',
-  buildTime: '2025-12-11T13:50:01.313Z',
-  hash: '4rke54vyogo'
+  version: '1765464258559',
+  buildTime: '2025-12-11T14:44:18.559Z',
+  hash: 'bfa3i0yndsv'
 };
 
 export default APP_VERSION;
