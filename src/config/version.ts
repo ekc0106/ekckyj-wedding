038a2bf4952ac6525452b1fb964e9b5e4f7521fe
ekc0106 <ekc0106@naver.com>
2025-12-12 02:19:41 +0900

    Trigger: Vercel 배포 트리거 및 빌드 테스트 완료
    - 새로운 갤러리 이미지들 정상 빌드 확인
    - 계좌정보, 주차안내, 비디오링크 변경사항 반영
    - 버전 업데이트: 44fudjebwpb

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,9 +1,9 @@
 // 이 파일은 빌드 시마다 자동으로 업데이트됩니다
-// Build Time: 2025-12-11T14:44:18.559Z
+// Build Time: 2025-12-11T17:19:15.892Z
 export const APP_VERSION = {
-  version: '1765464258559',
-  buildTime: '2025-12-11T14:44:18.559Z',
-  hash: 'bfa3i0yndsv'
+  version: '1765473555892',
+  buildTime: '2025-12-11T17:19:15.892Z',
+  hash: '44fudjebwpb'
 };
 
 export default APP_VERSION;
